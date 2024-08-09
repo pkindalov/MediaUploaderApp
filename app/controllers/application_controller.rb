5f4c248f86d84efbf4c4b33b82a025c106227307
pkindalov <panayotkindalov@gmail.com>
2024-08-09 23:30:42 +0300

    - Added devise gem for user login/registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  # Добавяне на хелпърите на Devise
+  helper_method :user_signed_in?, :current_user
 end
