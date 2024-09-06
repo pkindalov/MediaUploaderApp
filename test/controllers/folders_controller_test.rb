437f4edcb094bb90b16de4c1b4002ecdd8862ed3
pkindalov <panayotkindalov@gmail.com>
2024-09-06 13:40:36 +0300

    - Refactoring folders_controller_test.rb Working still in progress.

--- test/controllers/folders_controller_test.rb
+++ test/controllers/folders_controller_test.rb
@@ -4,70 +4,64 @@ require 'test_helper'
 
 class FoldersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    # @user = User.create!(email: 'testuser@example.com', password: 'password')
-    @user = User.find_by(email: 'user@example.com') || User.create!(email: 'user@example.com', password: 'password')
+    @user = users(:one)
+    sign_in @user
     @root_folder_path = Rails.configuration.user_files_path
     @user_main_folder_path = File.join(@root_folder_path, @user.email)
 
-    # Creating user's main directory
-    FileUtils.mkdir_p(@user_main_folder_path)
+    # Create a couple of parent folders (parent_id: nil)
+    @parent_folder1 = Folder.create!(name: 'Parent Folder 1', user: @user, parent_id: nil)
+    @parent_folder2 = Folder.create!(name: 'Parent Folder 2', user: @user, parent_id: nil)
 
-    # puts "User details: #{@user.inspect}"
-    # puts "User main directory: #{@user_main_folder_path}"
-    # puts Rails.configuration.user_files_path
+    # Create a child folder (parent_id set to one of the parent folders)
+    @child_folder = Folder.create!(name: 'Child Folder', user: @user, parent_id: @parent_folder1.id)
+
+    # @user = User.create!(email: 'testuser@example.com', password: 'password')
+    # @controller = FoldersController.new
+    # @user = User.find_by(email: 'user@example.com') || User.create!(email: 'user@example.com', password: 'password')
+    # @root_folder_path = Rails.configuration.user_files_path
+    # @user_main_folder_path = File.join(@root_folder_path, @user.email)
+
+    # Creating user's main directory
+    # FileUtils.mkdir_p(@user_main_folder_path)
   end
+
+  test 'should get index' do
+    get folders_path
+    assert_response :success
+    assert Dir.exist?(@user_main_folder_path)
+  end
+
+  test 'should fetch parent and child folders for current user' do
+    get folders_path
+    assert_response :success
+
+    # Fetch the assigned folders in the index view
+    folders = assigns(:folders)
+
+    # Ensure parent and child folders are included
+    assert_includes folders, @parent_folder1
+    assert_includes folders, @parent_folder2
+    assert_includes folders, @child_folder # Child folder should be in the flat list of folders
+  end
+
+
 
   # test 'experiment' do
   #   assert true
   # end
 
-  def teardown
-    FileUtils.rm_rf(@user_main_folder_path) if Dir.exist?(@user_main_folder_path)
-  end
-
-  test 'create one folder and one subfolder and then move the subfolder to the level of the folder' do
-    # Create the main folder in the database and on the flash drive
-    @main_test_folder = 'Documents'
-    @user_documents_folder = @user.folders.create!(name: @main_test_folder)
-    assert @user_documents_folder.persisted?
-
-    @main_test_folder_root = File.join(@user_main_folder_path, @main_test_folder)
-    FileUtils.mkdir_p(@main_test_folder_root)
-    assert Dir.exist?(@main_test_folder_root), 'Main test folder was not created on the file system'
-
-    # Create the subfolder in the database and on the file system
-    @sub_test_folder = 'Documents_Subfolder'
-    @sub_test_folder_root = File.join(@main_test_folder_root, @sub_test_folder)
-    @user_doc_subfolder = @user.folders.create!(name: @sub_test_folder, parent_id: @user_documents_folder.id)
-    assert @user_doc_subfolder.persisted?
-
-    FileUtils.mkdir_p(@sub_test_folder_root)
-    assert Dir.exist?(@sub_test_folder_root), 'Subfolder was not created on the file system'
-
-    # Move the subfolder to the top-level and update the database
-    @user_doc_subfolder.update!(parent_id: nil)
-    assert_nil @user_doc_subfolder.parent_id, 'Expected parent_id to be nil after update'
-
-    FileUtils.mv(@sub_test_folder_root, File.join(@user_main_folder_path, @sub_test_folder))
-    assert Dir.exist?(File.join(@user_main_folder_path, @sub_test_folder)), 'Subfolder was not moved to the top-level directory'
-
-  end
-
-
-  # test 'create one folder and several subfolders and then move the last subfolder on the level of the main folder' do
-  #
+  # def teardown
+  #   FileUtils.rm_rf(@user_main_folder_path) if Dir.exist?(@user_main_folder_path)
   # end
-  #
-  # test 'create one folder and several subfolders and then move the last subfolder one level up' do
-  #
-  # end
-  #
-  # test 'create one folder and several subfolders and then move the not last subfolder one level up' do
-  #
-  # end
-  #
-  # test 'create one folder and several subfolders and then move the not last subfolder one level of the main folder' do
-  #
+
+  # def create_folder_structure(base_folder, subfolders)
+  #   subfolders.each do |subfolder|
+  #     path = File.join(base_folder, subfolder)
+  #     FileUtils.mkdir_p(path)
+  #     assert Dir.exist?(path)
+  #     puts "Created folder: #{path}"
+  #   end
   # end
 
 end
